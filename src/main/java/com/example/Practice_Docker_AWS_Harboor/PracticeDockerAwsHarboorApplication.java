8dcc2bfecf2189f93ad40e74865a8874136a0ab4
shiwasmi <sagarchattar@gmail.com>
2026-07-06 17:05:05 +0530

    Code skealoton

---
+++ src/main/java/com/example/Practice_Docker_AWS_Harboor/PracticeDockerAwsHarboorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Practice_Docker_AWS_Harboor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDockerAwsHarboorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeDockerAwsHarboorApplication.class, args);
+	}
+
+}
